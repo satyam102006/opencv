bc7c04cf7175591032b4e828e4b28cff365856d9
satyam yadav <satyamyadavruler@gmail.com>
2026-01-27 19:06:11 +0530

    Update deep_features.hpp

--- modules/features/include/opencv2/deep_features.hpp
+++ modules/features/include/opencv2/deep_features.hpp
@@ -2,7 +2,7 @@
 #define OPENCV_FEATURES_DEEP_FEATURES_HPP
 
 #include "opencv2/core.hpp"
-#include "opencv2/features2d.hpp"
+#include "opencv2/features2d.hpp" 
 
 namespace cv {
 
@@ -10,7 +10,7 @@ namespace cv {
  *
  * Wrapping the inference of the ONNX model provided by LightGlue-ONNX.
  */
-class CV_EXPORTS_W DISK : public Feature2D
+class CV_EXPORTS DISK : public Feature2D // Changed from CV_EXPORTS_W to CV_EXPORTS
 {
 public:
     /**
@@ -18,9 +18,9 @@ public:
      * @param backendId The DNN backend to use (default: DNN_BACKEND_DEFAULT).
      * @param targetId The DNN target to use (default: DNN_TARGET_CPU).
      */
-    CV_WRAP static Ptr<DISK> create(const String& modelPath,
-                                    int backendId = 0,  // Use integers to avoid binding issues with enums if dnn.hpp isn't fully parsed yet
-                                    int targetId = 0);
+    static Ptr<DISK> create(const String& modelPath,
+                            int backendId = 0,
+                            int targetId = 0); // Removed CV_WRAP
 
     virtual String getDefaultName() const CV_OVERRIDE;
 };
